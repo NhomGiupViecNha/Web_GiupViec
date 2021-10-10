b5d2fe7991b72cfa38712a5b120d72eaf1523003
hungtran711 <81142650+hungtran711@users.noreply.github.com>
2021-10-10 19:18:54 +0700

    update phần admin

---
+++ Web_GiupViecNha/Areas/Admin/Controllers/DichVuController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_GiupViecNha.Areas.Admin.Models;
+namespace Web_GiupViecNha.Areas.Admin.Controllers
+{
+    public class DichVuController : Controller
+    {
+        //
+        // GET: /Admin/DichVu/
+        DBGiupViecNhaDataContext db = new DBGiupViecNhaDataContext();
+        //
+        // GET: /Admin/DichVu/
+
+        public ActionResult Index()
+        {
+            if (Session["nv"] == null)
+            {
+                return RedirectToAction("DangNhap", "TaiKhoan");
+
+            }
+            else
+            {
+                NhanVien nv = db.NhanViens.FirstOrDefault(a => a.MaNV == Session["nv"].ToString());
+
+                List<DichVu> dsdv = db.DichVus.ToList();
+                ViewData["dsdv"] = dsdv;
+                return View(nv);
+            }
+
+
+        }
+
+        [HttpGet]
+        public ActionResult ThongTinChiTietDV(string madv)
+        {
+            if (Session["nv"] == null)
+            {
+                return RedirectToAction("DangNhap", "TaiKhoan");
+
+            }
+            else
+            {
+                List<LoaiDV> ldv = db.LoaiDVs.ToList();
+                ViewBag.DSLoaiDV = ldv;
+                DichVu dv = db.DichVus.FirstOrDefault(a => a.MaDichVu == madv);
+                ViewData["dichvu"] = dv;
+                NhanVien nv = db.NhanViens.FirstOrDefault(a => a.MaNV == Session["nv"].ToString());
+
+                return View(nv);
+            }
+
+
+
+
+        }
+
+        [HttpPost]
+        public ActionResult ThongTinChiTietDV(FormCollection c, string madv)
+        {
+
+            DichVu dichvu = db.DichVus.FirstOrDefault(a => a.MaDichVu == madv);
+            dichvu.TenDichVu = c["txtTenDV"];
+            dichvu.MoTa = c["txtMoTa"];
+            dichvu.KinhNghiemYeuCau = c["txtKNYC"];
+            dichvu.DonViTinh = c["txtDVT"];
+            dichvu.GiaThanh = int.Parse(c["txtGia"].ToString());
+            dichvu.HinhAnh = c["txtHinhAnh"];
+            dichvu.LoaiDV = c["cbLoaiDV"];
+            UpdateModel(dichvu);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        [HttpGet]
+
+        public ActionResult ThemDichVu()
+        {
+            if (Session["nv"] == null)
+            {
+                return RedirectToAction("DangNhap", "TaiKhoan");
+
+            }
+            else
+            {
+                NhanVien nv = db.NhanViens.FirstOrDefault(a => a.MaNV == Session["nv"].ToString());
+                List<LoaiDV> ldv = db.LoaiDVs.ToList();
+                ViewBag.DSLoaiDV = ldv;
+                List<DichVu> dsdv = db.DichVus.ToList();
+                ViewData["dsdv"] = dsdv;
+                return View(nv);
+            }
+
+
+        }
+        [HttpPost]
+
+        public ActionResult ThemDichVu(FormCollection c)
+        {
+            return View();
+
+        }
+
+
+	}
+}
---
+++ Web_GiupViecNha/Areas/Admin/Controllers/TaiKhoanController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_GiupViecNha.Areas.Admin.Models;
+namespace Web_GiupViecNha.Areas.Admin.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        DBGiupViecNhaDataContext db = new DBGiupViecNhaDataContext();
+        //
+        // GET: /Admin/TaiKhoan/
+    
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection c)
+        {
+
+            NhanVien nv = db.NhanViens.FirstOrDefault(a => a.Email.Trim() == c["email"]);
+            {
+                if (nv.MatKhau.Trim() == c["password"])
+                {
+                    Session["nv"] = nv.MaNV;
+                    Response.Write(@"<script language='javascript'>alert('Message: \n" + "Đăng nhập thành công !!" + " .');</script>");
+                    return RedirectToAction("Index", "MainAdmin");
+                }
+
+            }
+            return View();
+        }
+
+        public ActionResult DangXuat()
+        {
+            Session["nv"] = null;
+
+            return RedirectToAction("DangNhap", "TaiKhoan");
+        }
+	}
+}
